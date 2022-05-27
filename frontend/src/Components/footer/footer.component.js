f0526ed5f4aeb618382a04916e364b1456dcf4af
Pierre-Alexandre Delgado <pierre-alexandre.delgado-arevalo@epitech.eu>
2022-05-27 12:10:49 +0200

    [Add] new component Footer

---
+++ frontend/src/Components/footer/footer.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './footer.component.css';
+
+const Footer = () => {
+
+return (
+	<footer className="list-footer">
+		<p>Todo list</p>
+	</footer>
+);
+};
+
+export default Footer;
